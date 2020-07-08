6313a342956bdaf103d114e38682664ae25826bf
AkankshaLodh01 <68018636+AkankshaLodh01@users.noreply.github.com>
2020-07-08 20:29:43 +0530

    created fibonacci.py
    This file prints the fibonacci series till whatever term you enter

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+number = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if number <= 0:
+   print("Please enter a positive integer")
+elif number == 1:
+   print("Fibonacci sequence upto",number,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < number:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
