57ce2dddc247e0d8bc178a47c4e250aa5de426c1
AkankshaLodh01 <68018636+AkankshaLodh01@users.noreply.github.com>
2020-07-08 20:45:22 +0530

    created positive.py
    this code prints the positive numbers from a given list

---
+++ positive.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for i in list1:
+    if i>0:
+        print(i)
+        
+        i+=1
+        
+for j in list2:
+    if j>0:
+        print(j)
+        j+=1
